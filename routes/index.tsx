520a73ac8a00c35208ec00687fccf6b6fc8e87ab
Eric Moore <eric.d.moore@gmail.com>
2022-10-13 15:47:50 -0500

    AppShell;

--- routes/index.tsx
+++ routes/index.tsx
@@ -2,33 +2,26 @@ import type {Handlers} from '$fresh/server.ts'
 import Counter from "../islands/Counter.tsx";
 import { TopHatBlack } from '../islands/TopHat.tsx';
 import { Head } from '$fresh/runtime.ts'
+import AppShell from '../components/AppShell.tsx'
 
 export default function Home() {
   return (
-      <div class="p-4 mx-auto max-w-screen-md">
-        <TopHatBlack title='Federa'  description='descrbes federa' icon="/logo.svg" >
-          <meta name="og:title" content="Federa" />
-        </TopHatBlack>
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
-        </p>
-        <ul>
-          <li><a href="/ast">ast</a></li>
-          <li><a href="/ast/http://example.com">ast/url</a></li>
-          <li><a href="/ast/composition/http://example.com">ast/composition/url</a></li>
-          <li><a href="/user">user</a></li>
-          <li><a href="/user/register">user/register</a></li>
-          <li><a href="/login">login</a></li>
-          <li><a href="/logout">logout</a></li>
-        </ul>
-      </div>
-    
+  <AppShell>
+    <TopHatBlack title='Federa' description='Descrbes federa' icon="/logo.svg" >
+      <meta name="og:title" content="Federa" />
+    </TopHatBlack>
+    <div class="p-4 mx-auto max-w-screen-md">
+      <ul>
+        <li><a href="/ast">ast</a></li>
+        <li><a href="/ast/http://example.com">ast/url</a></li>
+        <li><a href="/ast/composition/http://example.com">ast/composition/url</a></li>
+        <li><a href="/user">user</a></li>
+        <li><a href="/user/register">user/register</a></li>
+        <li><a href="/login">login</a></li>
+        <li><a href="/logout">logout</a></li>
+      </ul>
+    </div>
+  </AppShell>
   );
 }
 
