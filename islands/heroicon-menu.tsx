23aee144294c7826a9b16b114ee952fe53edebba
Eric Moore <eric.d.moore@gmail.com>
2022-10-25 14:43:10 -0500

    menu title toggle

--- islands/heroicon-menu.tsx
+++ islands/heroicon-menu.tsx
@@ -1,51 +1,53 @@
-import { JSX,  } from 'preact';
-import {useState, useRef, useEffect} from 'preact/hooks'
+import { JSX } from 'preact';
+import {useState, useId, useRef, useEffect} from 'preact/hooks'
 import outlineHeroIncons from '../components/heroicons/outline.tsx'
 import solidHeroIncons from '../components/heroicons/solid.tsx'
 
 export function Menu (){
-
     const solidEntries :[string, JSX.Element][] = Object.entries(solidHeroIncons)
     const outlineEntries :[string, JSX.Element][] = Object.entries(outlineHeroIncons)
 
-    // const ref = useRef(null)
+    const captionRef = useRef(null as null | HTMLParagraphElement);  
     const [elemHovered, setElemHovered]  = useState(0)
     const [showOutline, setShowOutline]  = useState(true)
-
-    const onClick = ()=>{ setShowOutline(!showOutline) }
-    const showCaption = (i:number)=>()=>{ setElemHovered(i) }
+        
+    const toggleOutlinesAndSolids = () => { setShowOutline(!showOutline) }
+    const showCaption = (i:number) => () => { setElemHovered(i) }
     
-    // const showCaption = useEffect(()=>{
-    //     const elem = ref.current
-    //     if(elem){
-    //         elem.style.setProperty('--caption', elemHovered)
+    // const onHover = () => useEffect(()=>{
+    //     if(captionRef.current) { captionRef.current.style.setProperty('color','black') }
+    //     return ()=>{
+    //         if(captionRef.current){ captionRef.current.style.setProperty('color', 'white')}
     //     }
-    // },[elemHovered])
-    
+    // },[captionRef])
+        
     return (
         <div class='container  mx-auto px-20 my-40 max-w-lg'>
-            <div class="flex flex-row justify-center">
+            <div class={`${showOutline? 'text-gray-300' : 'text-gray-800'} flex flex-row justify-center`}
+                onClick={toggleOutlinesAndSolids} >
                 <h1 class='flex mb-10 text-5xl'>Hero Icons</h1>
             </div>
             <div class="grid grid-cols-5 md:grid-cols-8 lg:grid-cols-10 xl:grid-cols-15 gap-8">
                 { outlineEntries.map(([name, outlineComp], i)=>{
                     return (
-                        <div class="flex flex-col flex-auto">
-                            <div class={`${showOutline ? '': 'hidden'} h-8 w-8 hover:text-blue-600`}
-                                onClick={onClick} 
+                        <div key={i} class="flex flex-col flex-auto">
+                            <div 
                                 onMouseOver={showCaption(i)}
+                                onTouchEnd={showCaption(i)}
+                                class={`${showOutline ? '': 'hidden'} h-8 w-8 hover:text-blue-600`}
                                 >
                                 {outlineComp } 
                             </div>
-                            <div class={`${showOutline ? 'hidden': ''} h-8 w-8 hover:text-blue-600`}
-                                onClick={onClick} 
+                            <div onClick={toggleOutlinesAndSolids} 
                                 onMouseOver={showCaption(i)}
+                                onTouchEnd={showCaption(i)}
+                                class={`${showOutline ? 'hidden': ''} h-8 w-8 hover:text-blue-600`}
                                 > 
                                 {solidEntries[i][1]}
                             </div>
-                            {/* text-white hover:text-black */}
-                            <p class={`${i === elemHovered ? 'text-base' : 'text-white'}`}
+                            <p ref={captionRef} class={`${i === elemHovered ? 'text-base' : 'text-white'}`}
                                 onMouseOver={showCaption(i)}
+                                onTouchEnd={showCaption(i)}
                             > {name} </p>
                         </div>)
                 })}
