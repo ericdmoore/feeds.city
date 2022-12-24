cd447e4d75110d396896fa1071c6b70740308135
Eric D Moore <eric.d.moore@gmail.com>
2022-12-23 21:23:33 -0600

    lock, vs-code extensions file, etc

--- twind.config.ts
+++ twind.config.ts
@@ -1,4 +1,4 @@
-import { Options } from "$fresh/plugins/twind.ts";
+import type { Options } from "$fresh/plugins/twind.ts";
 // import { forms } from "@twind/forms";
 // import { aspectRatio } from "@twind/aspect-ratio";
 
--- utils/ECkeys/gen.ts
+++ utils/ECkeys/gen.ts
@@ -1,10 +1,13 @@
+
 const keypair = await crypto.subtle.generateKey(
   { name: "ECDSA", namedCurve: "P-384" },
   true,
   ["sign", "verify"],
 ) as CryptoKeyPair;
+
 console.log(
   "\n\n Private key:",
+  {kid: Date.now()},
   JSON.stringify(
     await crypto.subtle.exportKey("jwk", keypair.privateKey),
     null,
@@ -13,6 +16,7 @@ console.log(
 );
 console.log(
   "\n\n Public key:",
+  {kid: Date.now()},
   JSON.stringify(
     await crypto.subtle.exportKey("jwk", keypair.publicKey),
     null,
