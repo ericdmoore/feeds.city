d12b55f46cb5ffb7cfa527ac44fd182d9c4ea30c
Eric D Moore <eric.d.moore@gmail.com>
2023-08-09 20:51:38 -0500

    update

--- lib/utils/vars.ts
+++ lib/utils/vars.ts
@@ -2,8 +2,13 @@ import { resolve } from "$std/path/mod.ts";
 import { parse } from "$std/dotenv/mod.ts";
 
 export const envVar = async (defaultVal: string, path = "../../../.env") => {
+	
 	const p = resolve(import.meta.url, path).split(":")[1]; // crazy `file:` prefix after import.meta.url
-	const stat = await Deno.stat(p).catch(()=>({isFile: false}))
+	const stat = await Deno.stat(p)
+		.then(s => s,()=>({isFile: false}))
+		.catch(()=>({isFile: false}))
+
+	console.log({p, stat})
 
 	if(stat.isFile){
 		const encFileBytes = await Deno.readFile(p)
@@ -22,7 +27,7 @@ export const envVar = async (defaultVal: string, path = "../../../.env") => {
 	}else{
 		console.error(`NO .env FILE FOUND at: ${p}`)
 		console.error(`falling back to "Deno.env.get" + the good ol default: ${defaultVal}`)
-		return (key:string)=>Deno.env.get(key) ?? defaultVal
+		return (key:string) => Deno.env.get(key) ?? defaultVal
 	}
 };
 
